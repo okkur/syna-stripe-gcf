f77d6e1b8ded2bb72816a7b932341c26e59c76de
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-01-29 14:59:09 +0330

    Run prettier on server files

--- create-customer/index.js
+++ create-customer/index.js
@@ -1,23 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var stripe = require('stripe')(
-  process.env.STRIPE_KEY || "your_stripe_key"
-);
+var stripe = require('stripe')(process.env.STRIPE_KEY || 'your_stripe_key');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //create a customer w/ card on file
-exports.createCustomer = app.get(".../:email/:desc/:tok", function createCustomer (req,res){
-  var customer = stripe.customers.create({
-    email: req.params.email,
-    description: req.params.desc,
-    card: req.params.tok,
-  }, function(err, customer) {
-    if(err) {
-      return res.send(JSON.stringify(err));
-    }
-    res.send(customer);
-  });
-});
+exports.createCustomer = app.get(
+  '.../:email/:desc/:tok',
+  function createCustomer(req, res) {
+    var customer = stripe.customers.create(
+      {
+        email: req.params.email,
+        description: req.params.desc,
+        card: req.params.tok,
+      },
+      function(err, customer) {
+        if (err) {
+          return res.send(JSON.stringify(err));
+        }
+        res.send(customer);
+      },
+    );
+  },
+);
